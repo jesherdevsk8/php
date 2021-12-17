12dc674365f866ec76bd14edcdbc22237538dc15
Jesher Minelli <jesherdevsk8@gmail.com>
2021-12-16 21:28:28 -0300

    update

--- php-aulas/php-aula05/aula05/funcoesaritmeticas.php
+++ php-aulas/php-aula05/aula05/funcoesaritmeticas.php
@@ -21,7 +21,7 @@
         echo "O valor absoluto de $v2 e " . abs($v2);
         echo "<br/>O valor de $v1<sup>$v2</sup> e " . pow($v1, $v2);
         echo "<br/>A raiz de $v1 e " . sqrt($v1);
-        echo "<br/>O valor de $v2 arredondado e " . round($v2); // ceil floor
+        echo "<br/>O valor de $v2 arredondado e " . round($v2); // floor() arredonda um valor para baixo
         echo "<br/>A parte inteira de $v2 e " . intval($v2);
         echo "<br/>O valor de $v1 em moeda e R$" . number_format($v1,2,",", ".");
     ?>
